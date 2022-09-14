f4a715f892df3f6ec42edd969248207c4091399b
wesleymktd <wesleymktd@gmail.com>
2022-09-14 18:08:50 -0300

    terminei a 4

---
+++ script.js
@@ -0,0 +1,17 @@
+let getEmail = document.querySelector('.email');
+let getPassword = document.querySelector('.password');
+let getButton = document.querySelector('button');
+
+function validationUser() {
+  let mailVerify = getEmail.value;
+  let passVerify = getPassword.value;
+  
+  if (mailVerify === 'tryber@teste.com' && passVerify === '123456') {
+    window.alert('Olá, Tryber!');
+  } else {
+    window.alert('Email ou senha inválidos.')
+  }
+}
+
+getButton.addEventListener('click',validationUser)
+
