af73fdcaf714e8bd3724df87f3eb5061cb0c9d97
Vinicius <angelim7v@gmail.com>
2022-09-14 23:06:49 -0300

    adicionados requisitos 13 e 14

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-let getEmail = document.querySelector('.email');
-let getPassword = document.querySelector('.password');
-let getButton = document.querySelector('button');
+const getEmail = document.querySelector('.email');
+const getPassword = document.querySelector('.password');
+const getButton = document.querySelector('button');
 function validationUser() {
-  let mailVerify = getEmail.value;
-  let passVerify = getPassword.value;
+  const mailVerify = getEmail.value;
+  const passVerify = getPassword.value;
   if (mailVerify === 'tryber@teste.com' && passVerify === '123456') {
     window.alert('Olá, Tryber!');
   } else {
-    window.alert('Email ou senha inválidos.')
+    window.alert('Email ou senha inválidos.');
   }
 }
-getButton.addEventListener('click', validationUser)
+getButton.addEventListener('click', validationUser);
